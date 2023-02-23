3bb656ac21b83f1ddc9e5f72983ef15527235265
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-02-23 12:40:10 +0100

    Cadeal

---
+++ app/header.js
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+================================================== -->
+    <meta charset="utf-8" />
+    <title>Constra - Construction Html5 Template</title>
+
+    <!-- Mobile Specific Metas
+================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Construction Html5 Template" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
+    />
+    <meta name="author" content="Themefisher" />
+    <meta name="generator" content="Themefisher Constra HTML Template v1.0" />
+
+    <!-- Favicon
+================================================== -->
+    <link rel="icon" type="image/png" href="#" />
+
+    <!-- CSS
+================================================== -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css" />
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css" />
+    <!-- Animation -->
+    <link rel="stylesheet" href="plugins/animate-css/animate.css" />
+    <!-- slick Carousel -->
+    <link rel="stylesheet" href="plugins/slick/slick.css" />
+    <link rel="stylesheet" href="plugins/slick/slick-theme.css" />
+    <!-- Colorbox -->
+    <link rel="stylesheet" href="plugins/colorbox/colorbox.css" />
+    <!-- Template styles-->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <div class="body-inner">
+      <div id="top-bar" class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-8">
+              <ul class="top-info text-center text-md-left">
+                <li>
+                  <i class="fas fa-map-marker-alt"></i>
+                  <p class="info-text">Ikeja, Lagos, Nigeria</p>
+                </li>
+              </ul>
+            </div>
+            <!--/ Top info end -->
+
+            <div class="col-lg-4 col-md-4 top-social text-center text-md-right">
+              <ul class="list-unstyled">
+                <li>
+                  <a title="Facebook" href="#">
+                    <span class="social-icon"
+                      ><i class="fab fa-facebook-f"></i
+                    ></span>
+                  </a>
+                  <a title="Twitter" href="#">
+                    <span class="social-icon"
+                      ><i class="fab fa-twitter"></i
+                    ></span>
+                  </a>
+                  <a title="Instagram" href="#">
+                    <span class="social-icon"
+                      ><i class="fab fa-instagram"></i
+                    ></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <!--/ Top social end -->
+          </div>
+          <!--/ Content row end -->
+        </div>
+        <!--/ Container end -->
+      </div>
+      <!--/ Topbar end -->
+      <!-- Header start -->
+      <header id="header" class="header-two">
+        <div class="site-navigation">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light p-0">
+                  <div class="logo">
+                    <a class="d-block" href="index-2.html">
+                      <!-- <img loading="lazy" src="#" alt="Car rentals" /> -->
+                      <h1>Car Rentals</h1>
+                    </a>
+                  </div>
+                  <!-- logo end -->
+
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target=".navbar-collapse"
+                    aria-controls="navbar-collapse"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div id="navbar-collapse" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav ml-auto align-items-center">
+                      <li><a href="index.html">Home</a></li>
+                      <li><a href="about.html">About Us</a></li>
+                      <li>
+                        <a class="nav-link" href="contact.html">Contact</a>
+                      </li>
+
+                      <li class="header-get-a-quote">
+                        <a class="btn btn-primary" href="contact.html"
+                          >Rent A Car</a
+                        >
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+              <!--/ Col end -->
+            </div>
+            <!--/ Row end -->
+          </div>
+          <!--/ Container end -->
+        </div>
+        <!--/ Navigation end -->
+      </header>
+      <!--/ Header end --></div>
